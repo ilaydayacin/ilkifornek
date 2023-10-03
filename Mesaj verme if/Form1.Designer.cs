76f4bb81bc7975531b61cb84116cbcba65ba150a
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-10-03 15:22:19 +0300

    Add files via upload

---
+++ Mesaj verme if/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Mesaj_verme_if
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMesaj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMesaj
+            // 
+            this.btnMesaj.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnMesaj.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesaj.Location = new System.Drawing.Point(28, 12);
+            this.btnMesaj.Name = "btnMesaj";
+            this.btnMesaj.Size = new System.Drawing.Size(151, 143);
+            this.btnMesaj.TabIndex = 0;
+            this.btnMesaj.Text = "Mesaj ver";
+            this.btnMesaj.UseVisualStyleBackColor = false;
+            this.btnMesaj.Click += new System.EventHandler(this.btnMesaj_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 179);
+            this.Controls.Add(this.btnMesaj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMesaj;
+    }
+}
+
---
+++ Mesaj verme if/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mesaj_verme_if
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMesaj_Click(object sender, EventArgs e)
+        {
+            int yas = 20;
+            if (yas >= 18) 
+            {
+                MessageBox.Show("Ehliyet alabilirsiniz ");
+            }
+        }
+    }
+}
